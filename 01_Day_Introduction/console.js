dbf2fc7aa6fc144f983794f60ba9f67148d8dc35
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-08 23:51:36 -0400

    day one of my 100 days of code.

---
+++ 01_Day_Introduction/console.js
@@ -0,0 +1,12 @@
+console.log(firstName)
+console.log(lastName)
+console.log(country)
+console.log(city)
+console.log(age)
+console.log(isMarried)
+console.log(gravity)
+console.log(boilingPoint)
+console.log(PI)
+console.log(`Hi, my name is ${name}, 
+            I work as a ${job}, 
+            and I live in ${live}`)
