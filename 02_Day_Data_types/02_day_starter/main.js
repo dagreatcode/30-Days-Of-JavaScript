3c1188ffd0843e03e72000a3b90cebdcd342b2ca
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-10 23:03:32 -0400

    day 2

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,43 @@
-// this is your main.js script
+// let nums = [1, 2, 3]
+// let numbers = [1, 2, 3]
+
+// console.log(nums == numbers)  // false
+
+// let nums2 = [1, 2, 3]
+// let numbers2 = nums2
+
+// console.log(nums2 == numbers2)  // true
+
+var challange = ' 30 Day Of JavaScript'
+console.log(challange)
+console.log(challange.length)
+console.log(challange.toUpperCase())
+console.log(challange.toLocaleLowerCase())
+console.log(challange.substring(3,6))
+console.log(challange.slice(3,20))
+console.log(challange.includes('Script'))
+console.log(challange.split())
+var sites = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(sites.split(','))
+console.log(challange.replace('JavaScript', 'Python'))
+console.log(challange.charAt(15))
+console.log(challange.charCodeAt('j'))
+console.log(challange.indexOf('a'))
+console.log(challange.lastIndexOf('a'))
+console.log(challange.search(30))
+console.log(challange.trim(' '))
+console.log(challange.startsWith(' '))
+console.log(challange.endsWith('t'))
+console.log(challange.match('a'))
+console.log(challange.concat(' is only the start'))
+console.log(challange.repeat(2))
+
+console.log("\'The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.\'")
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+// console.log(motherTQ)
+// console.log(motherTQ.typeOf(4))
+console.log(typeof('10') === typeof (10).toString())
+console.log(parseFloat(Math.round('9.8')) === (10))
+var lag = 'python'
+var lag2 = 'jargon'
+console.log(lag2.search('on') === lag.search('on'))
